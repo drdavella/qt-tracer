3f927b789e1fbc9f8b6f5206b57f56b026930609
Daniel D'Avella <drdavella@gmail.com>
2018-05-07 19:39:28 -0400

    Register each widget at init time to store traceback info

--- qt-patch.py
+++ qt-patch.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import sys
+import inspect
 import importlib
 
 def patch_event(orig_func, new_func):
@@ -12,15 +13,33 @@ def patch_event(orig_func, new_func):
 def main():
 
     qtcore = importlib.import_module('qtpy.QtCore')
+    qtwidgets = importlib.import_module('qtpy.QtWidgets')
+
+    widget_registry = {}
+
+    def register_widget(*args, **kwargs):
+        widget = args[0]
+        frames = inspect.stack()
+        for i, frame_info in enumerate(reversed(frames)):
+            if frame_info.function == '__init__':
+                widget_registry[widget] = frames[i-1]
+                break
 
     def handle_mouse_click(*args, **kwargs):
-        self, obj, event = args[:3]
-        if event.type() == qtcore.QEvent.MouseButtonPress:
-            print(self, obj, event)
+        widget = args[0]
+        frame_info = widget_registry[widget]
+        info = "{}:{} {}".format(frame_info.filename,
+                                 frame_info.lineno,
+                                 frame_info.code_context)
+        print(widget, info)
 
-    qtcore.QObject.eventFilter = patch_event(qtcore.QObject.eventFilter, handle_mouse_click)
+    qtwidgets.QWidget.__init__ = patch_event(qtwidgets.QWidget.__init__,
+                                             register_widget)
+    qtwidgets.QWidget.mousePressEvent = patch_event(qtwidgets.QWidget.mousePressEvent,
+                                                    handle_mouse_click)
 
     sys.modules['qtpy.QtCore'] = qtcore
+    sys.modules['qtpy.QtWidgets'] = qtwidgets
 
     from cubeviz.cubeviz import main as cubeviz_main
     cubeviz_main()
