75eff9178b29d0ee7ac18f2893777f4e30483f3a
Daniel D'Avella <drdavella@gmail.com>
2018-05-07 12:13:15 -0400

    Commit current state before updating to use QWidget

--- qt-patch.py
+++ qt-patch.py
@@ -2,9 +2,9 @@
 import sys
 import importlib
 
-def patch_event(orig_func):
+def patch_event(orig_func, new_func):
     def patch_func(*args, **kwargs):
-        print(args, kwargs)
+        new_func(*args, **kwargs)
         return orig_func(*args, **kwargs)
 
     return patch_func
@@ -12,7 +12,13 @@ def patch_event(orig_func):
 def main():
 
     qtcore = importlib.import_module('qtpy.QtCore')
-    qtcore.QObject.__init__ = patch_event(qtcore.QObject.__init__)
+
+    def handle_mouse_click(*args, **kwargs):
+        self, obj, event = args[:3]
+        if event.type() == qtcore.QEvent.MouseButtonPress:
+            print(self, obj, event)
+
+    qtcore.QObject.eventFilter = patch_event(qtcore.QObject.eventFilter, handle_mouse_click)
 
     sys.modules['qtpy.QtCore'] = qtcore
 
